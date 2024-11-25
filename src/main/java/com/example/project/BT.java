b23f8543dd6763851a2ade3e2a47cb7a40008469
Administrator <Administrator@WINDOWS-3EJDPVL>
2024-11-25 13:24:05 +0300

    update tree user

--- src/main/java/com/example/project/BT.java
+++ src/main/java/com/example/project/BT.java
@@ -114,31 +114,7 @@ public class BT<T> {
 
 }
 
-	public static<T> int countLeafs(BT<T> bt ,T e) {
-		if(bt.empty())
-			return 0;
-		bt.find(Relative.Root);
-		return recCountLeafs(bt);
-	}
 
-
-private static <T> int recCountLeafs (BT<T> bt ) {
-	if (bt.isLeaf())
-		return 1; 
-	else { 
-	      int nb = 0;
-	      if(bt.find(Relative.LeftChild)){
-		   nb += recCountLeafs (bt);
-		   bt.find (Relative.Parent);
-		}
-	     if (bt.find(Relative.RightChild )){ 
-		     nb += recCountLeafs ( bt ) ;
-		     bt.find (Relative.Parent);
-	     }
-		return nb ;
-	
-	}
-}
 
 }
 
